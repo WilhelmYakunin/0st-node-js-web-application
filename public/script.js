dce3b938f5132f5607424814497a2f22270c384b
WilhelmYakunin <v.jakunin@inbox.ru>
2020-05-15 14:04:56 +0300

    refactoring

--- public/script.js
+++ public/script.js
@@ -1,11 +1,14 @@
 'use strict';
 
+// MENU BUTTON TRANSITION //
 let btn = document.querySelector('.menu-container-btn');
 let firstSpan = document.querySelector('span:first-child');
 let secondSpan = document.querySelector('span:nth-child(2)');
 let lastSpan = document.querySelector('span:last-child');
 let siteNavagation = document.querySelector('.site-navagation');
 let logo = document.querySelector('.menu-container-logo p');
+let linkToHomePage = document.querySelector('.linkToHomepage');
+
 let count = 0;
 
 btn.addEventListener('click', function (evt) {
@@ -20,36 +23,38 @@ btn.addEventListener('click', function (evt) {
       firstSpan.classList.toggle("btn-transition-first-back");
       secondSpan.classList.toggle("btn-transition-second-back");
       lastSpan.classList.toggle("btn-transition-last-back");
-      siteNavagation.classList.toggle("un-visible");
+      siteNavagation.classList.toggle("visible");
     } return count--;
 });
 
+// STRUCTURE OF INFORMATION HIDE //
 let introInfoStructure = document.querySelector('.structure');
 
 function hideInroInfoDiv () {
   introInfoStructure.classList.add("un-visible");
  };
  
-setTimeout(hideInroInfoDiv, 5000);
+setTimeout(hideInroInfoDiv, 3000);
 
+// PAPER PROTOTYPE HIDE //
 let paperPrototypeContainer = document.querySelector('.paper-prototype-container');
 
 function hideInroPaperPrototypeDiv () {
   paperPrototypeContainer.classList.add("un-visible");
 };
 
-setTimeout(hideInroPaperPrototypeDiv, 5000);
+setTimeout(hideInroPaperPrototypeDiv, 3000);
 
-
+// INTERACTIVE PROTOTYPE ACTION TYPING //
 let prototypeText = document.querySelector('.interactive-portotype-text');
 let arr = ['и','н', 'т', 'е', 'р', 'а', 'к', 'т', 'и', 'в', 'н', 'ы', 'й',
           '&nbsp;', 'п', 'р', 'о', 'т', 'о', 'т', 'и', 'п'];
-let delay = 150;
+let delay = 110;
 for(let i = 0; i <arr.length; i += 1) {
     setTimeout(function(i) {
         return function() {
             let span = document.createElement('p');
-            span.classList.add("big-grey");
+            span.classList.add("big-red");
           span.innerHTML = `${arr[i]}`;
           prototypeText.before(span);
         };
@@ -62,8 +67,9 @@ function hideInteractivePortotypeDiv () {
   interactivePortotype.classList.add("un-visible");
 };
 
-setTimeout(hideInteractivePortotypeDiv, 5000);
+setTimeout(hideInteractivePortotypeDiv, 3000);
 
+// LINKS ACTIVE STYLE //
 let links = document.querySelectorAll("nav a");
 let aHover = ['#46ac4e', '#3fade0', '#ffca30', '#df386e'];
 
